89186319a0cfc32f8224c2ec1ed9b5fc23f16af6
Wouter Bles <whbles@gmail.com>
2021-04-08 18:22:11 +0200

    Fix client on windows

--- client.py
+++ client.py
@@ -4,13 +4,14 @@ from tests.optimization_models import (
     unit_commitment_model, four_kp_model, three_kp_model, two_kp_model,
     three_objective_model, two_objective_model)
 
-options = {
-    'grid_points': 540,
-    'nadir_points': [1031, 1069],
-    'early_exit': True,  # AUGMECON
-    'bypass_coefficient': True,  # AUGMECON2
-    'flag_array': True,  # AUGMECON-R
-    }
+if __name__ == '__main__':
+    options = {
+        'grid_points': 540,
+        'nadir_points': [1031, 1069],
+        'early_exit': True,  # AUGMECON
+        'bypass_coefficient': True,  # AUGMECON2
+        'flag_array': True,  # AUGMECON-R
+        }
 
-model_type = '3kp40'
-A = MOOP(three_kp_model(model_type), options, model_type)
+    model_type = '3kp40'
+    A = MOOP(three_kp_model(model_type), options, model_type)
