05ad53c7866423642f3277ec0ea38644c749ad7c
Wouter Bles <whbles@gmail.com>
2021-05-23 14:51:21 +0200

    Add temporary excel output to client

--- client.py
+++ client.py
@@ -6,43 +6,46 @@ from tests.optimization_models import (
     three_objective_model, two_objective_model)
 
 if __name__ == '__main__':
-    model_type = '3kp40'
+    model_type = 'three'
 
     options = {
         'name': model_type,
-        'grid_points': 540,
-        'nadir_points': [1031, 1069],
+        'grid_points': 10,
+        # 'nadir_points': [1031, 1069],
         'early_exit': True,  # AUGMECON
-        'bypass_coefficient': True,  # AUGMECON2
-        'flag_array': True,  # AUGMECON-R
+        'bypass_coefficient': False,  # AUGMECON2
+        'flag_array': False,  # AUGMECON-R
         'shared_flag': True,
-        #'nadir_ratio': 0.05,
-        'redivide_work': False,
-        'round_decimals': 0,
-        #'cpu_count': 1,
+        #'nadir_ratio': 0.9,
+        # 'redivide_work': True,
+        #'round_decimals': 0,
+        'cpu_count': 1,
         }
 
     solver_options = {
-        'Threads': 1,
+        # 'Threads': 1,
     }
 
-    A = PyAugmecon(three_kp_model(model_type), options, solver_options)
+    A = PyAugmecon(three_objective_model(), options, solver_options)
     A.solve()
-    
+
     print('--- PAY-OFF TABLE ---')
     print(A.model.payoff)
-    print('--- E-POINTS ---')
-    print(np.sort(A.model.e))
-    print('--')
-    print(A.model.obj_range)
-    print('--- PARETO SOLS ---')
-    print(A.pareto_sols.shape)
-    print('--')
-    print(A.pareto_sols)
-    print('--- MODELS SOLVED ---')
-    print(A.model.models_solved.value())
-    print('--- INFEASIBILITIES ---')
-    print(A.model.infeasibilities.value())
+    #print('--- E-POINTS ---')
+    #print(np.sort(A.model.e))
+    #print('--')
+    #print(A.model.obj_range)
+    #print('--- PARETO SOLS ---')
+    #print(A.pareto_sols.shape)
+    #print('--')
+    #print(A.pareto_sols)
+    #print('--- MODELS SOLVED ---')
+    #print(A.model.models_solved.value())
+    #print('--- INFEASIBILITIES ---')
+    #print(A.model.infeasibilities.value())
 
-    pd.DataFrame(A.pareto_sols).to_excel(
-        f'{A.opts.logdir}/{A.opts.name}.xlsx')
+    writer = pd.ExcelWriter(f'{A.opts.logdir}/{A.opts.name}.xlsx')
+    pd.DataFrame(A.pareto_sols).to_excel(writer, 'pareto_sols')
+    pd.DataFrame(A.model.e).to_excel(writer, 'e_points')
+    pd.DataFrame(A.model.payoff).to_excel(writer, 'payoff_table')
+    writer.save()
