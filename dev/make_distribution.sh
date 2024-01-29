8195f30a05e2c399bccefed7395409368b28972c
Wouter Bles <whbles@gmail.com>
2024-01-29 12:55:05 +0100

    Update deps

--- dev/make_distribution.sh
+++ dev/make_distribution.sh
@@ -6,8 +6,12 @@ set -e
 FWDIR="$(cd "`dirname "${BASH_SOURCE[0]}"`"; pwd)"
 cd "$FWDIR/.."
 
+# Clean up previous build artifacts
 rm -rf dist
 rm -rf pyaugmecon.egg-info
-python setup.py sdist
-python setup.py bdist_wheel
+
+# Build source and wheel distribution using the `build` package
+python -m build
+
+# Upload the distribution files to PyPI using twine
 twine upload dist/*
